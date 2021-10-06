a598e7a658b083b8a0ff639efdb168cbfd708990
vmartinir <vmartinir@aliyun.com>
2021-10-06 22:31:59 +0800

    Update readme.md

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 旨在记录dubbo编码中的常规使用，其中包括技术点如下：
 
-- 服务注册与发现：zookeeper
+- 服务注册与发现：zookeeper、nacos
--- spring-dubbo-provider/src/main/resources/bash/initialize.sh
+++ spring-dubbo-provider/src/main/resources/bash/initialize.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+# zookeeper部署
 docker run -d --name zookeeper \
 --restart always \
 -v /etc/localtime:/etc/localtime \
 -p 2181:2181 zookeeper:3.7.0
+
+# dubbo-admin部署
+docker run -d --name dubbo-admin \
+--restart always \
+-v /etc/localtime:/etc/localtime \
+-p 8080:8080 \
+-e admin.registry.address=zookeeper://192.168.71.128:2181 \
+-e admin.config-center=zookeeper://192.168.71.128:2181 \
+-e admin.metadata-report.address=zookeeper://192.168.71.128:2181 \
+apache/dubbo-admin
+
